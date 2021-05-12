67527ae125f3ff38af64292bbe8d591a7cba304e
ninja <kirillsurov0@gmail.com>
2021-05-13 01:20:46 +0900

    feat: delete prettier and add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -13,92 +13,92 @@ import del from 'del';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const postcssPlugins = [
-	postcssImport(),
-	postcssVars(),
-	postcssNested(),
-	postcssNestedProps(),
-	postcssMixin(),
-	autoprefixer(),
-	cssnano(),
+  postcssImport(),
+  postcssVars(),
+  postcssNested(),
+  postcssNestedProps(),
+  postcssMixin(),
+  autoprefixer(),
+  cssnano()
 ];
 
 const PATHS = {
-	styles: path.resolve(__dirname, 'src/assets/styles/main.css'),
-	image: path.resolve(__dirname, './src/assets/image/**/*'),
-	fonts: path.resolve(__dirname, 'src/assets/fonts/**/*'),
-	templates: path.resolve(__dirname, 'src/templates/**/*'),
+  styles: path.resolve(dirname, 'src/assets/styles/main.css'),
+  image: path.resolve(dirname, './src/assets/image/**/*'),
+  fonts: path.resolve(dirname, 'src/assets/fonts/**/*'),
+  templates: path.resolve(dirname, 'src/templates/**/*')
 };
 
 function cssTask() {
-	return gulp
-		.src(PATHS.styles)
-		.pipe(postcss(postcssPlugins))
-		.pipe(rename('main.min.css'))
-		.pipe(gulp.dest(path.resolve(__dirname, 'dist/assets/styles')));
+  return gulp
+    .src(PATHS.styles)
+    .pipe(postcss(postcssPlugins))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest(path.resolve(dirname, 'dist/assets/styles')));
 }
 
 function htmlTask() {
-	return gulp.src(PATHS.templates).pipe(gulp.dest(path.resolve(__dirname, 'dist/templates')));
+  return gulp.src(PATHS.templates).pipe(gulp.dest(path.resolve(dirname, 'dist/templates')));
 }
 
 function imageTask() {
-	return gulp
-		.src(PATHS.image)
-		.pipe(imageWebp({
-			quality: 40
-		}))
-		.pipe(gulp.dest(path.resolve(__dirname, 'dist/assets/image')));
+  return gulp
+    .src(PATHS.image)
+    .pipe(imageWebp({
+      quality: 40
+    }))
+    .pipe(gulp.dest(path.resolve(dirname, 'dist/assets/image')));
 }
 
 function fontsTask() {
-	return gulp.src(PATHS.fonts).pipe(gulp.dest(path.resolve(__dirname, 'dist/assets/fonts')));
+  return gulp.src(PATHS.fonts).pipe(gulp.dest(path.resolve(dirname, 'dist/assets/fonts')));
 }
 
 function watch() {
-	gulp.watch(['src/assets/styles/**/*'], gulp.parallel(cssTask));
-	gulp.watch(['src/assets/fonts/**/*'], gulp.parallel(fontsTask));
-	gulp.watch(['src/assets/image/**/*'], gulp.parallel(imageTask));
-	gulp.watch(['src/templates/**/*'], gulp.parallel(htmlTask));
+  gulp.watch(['src/assets/styles/**/*'], gulp.parallel(cssTask));
+  gulp.watch(['src/assets/fonts/**/*'], gulp.parallel(fontsTask));
+  gulp.watch(['src/assets/image/**/*'], gulp.parallel(imageTask));
+  gulp.watch(['src/templates/**/*'], gulp.parallel(htmlTask));
 }
 
 async function cleanBuild() {
-	let delPaths = await del(['./dist/**', '!./dist/js']);
+  let delPaths = await del(['./dist/**', '!./dist/js']);
 
-	return delPaths;
+  return delPaths;
 }
 
 async function cleanCssFonts() {
-	let delPaths = await del(['./dist/assets/fonts/**/*.css']);
+  let delPaths = await del(['./dist/assets/fonts/**/*.css']);
 
-	return delPaths;
+  return delPaths;
 }
 
 async function gulpWatch() {
-	await gulp
-		.series(
-			cleanBuild,
-			imageTask,
-			gulp.parallel(cssTask, htmlTask, fontsTask, imageTask),
-			watch
-		)
-		.call(this);
+  await gulp
+    .series(
+      cleanBuild,
+      imageTask,
+      gulp.parallel(cssTask, htmlTask, fontsTask, imageTask),
+      watch
+    )
+    .call(this);
 
-	return true;
+  return true;
 }
 
 async function gulpBuild() {
-	await gulp
-		.series(
-			cleanBuild,
-			gulp.parallel(cssTask, htmlTask, fontsTask, imageTask),
-			cleanCssFonts
-		)
-		.call(this);
+  await gulp
+    .series(
+      cleanBuild,
+      gulp.parallel(cssTask, htmlTask, fontsTask, imageTask),
+      cleanCssFonts
+    )
+    .call(this);
 
-	return true;
+  return true;
 }
 
 export { gulpWatch, gulpBuild };
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,55 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import ESLintPlugin from 'eslint-webpack-plugin';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const PATHS = {
-	appPoint: path.resolve(__dirname, 'src/assets/js/app.js'),
-	appOutput: path.resolve(__dirname, 'dist/assets/js/'),
+  appPoint: path.resolve(dirname, 'src/assets/js/app.js'),
+  appOutput: path.resolve(dirname, 'dist/assets/js/')
 };
 
-export default (env, args) => {
+export default () => {
+  return {
+    entry: {
+      app: PATHS.appPoint
+    },
 
-	return {
-		entry: {
-			app: PATHS.appPoint,
-		},
+    output: {
+      filename: 'app.build.js',
+      path: PATHS.appOutput
+    },
 
-		output: {
-			filename: 'app.build.js',
-			path: PATHS.appOutput,
-		},
+    resolve: {
+      modules: ['node_modules']
+    },
 
-		resolve: {
-			modules: ['node_modules'],
-		},
+    module: {
+      rules: [
+        {
+          test: /\.js$/i,
+          use: [
+            {
+              loader: 'cache-loader',
+              options: {
+                cacheDirectory: path.resolve(
+                  __dirname,
+                  '.cache/js-cache'
+                )
+              }
+            },
+            'babel-loader'
+          ]
+        }
+      ]
+    },
 
-		module: {
-			rules: [
-				{
-					test: /\.js$/i,
-					use: [
-						{
-							loader: 'cache-loader',
-							options: {
-								cacheDirectory: path.resolve(
-									__dirname,
-									'.cache/js-cache'
-								),
-							},
-						},
-						'babel-loader',
-					],
-				},
-			],
-		},
+    watchOptions: {
+      aggregateTimeout: 100
+    },
 
-		// Options
-
-		watchOptions: {
-			aggregateTimeout: 100,
-		},
-	};
+    plugins: [
+      new ESLintPlugin()
+    ]
+  };
 };
