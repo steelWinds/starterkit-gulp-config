4a8f8f62d2e35764af353159e1f39b789e02ab34
ninja <kirillsurov0@gmail.com>
2021-05-13 20:52:49 +0900

    fix(lint): fix spaces indent

--- webpack.config.js
+++ webpack.config.js
@@ -5,51 +5,51 @@ import ESLintPlugin from 'eslint-webpack-plugin';
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const PATHS = {
-  appPoint: path.resolve(dirname, 'src/assets/js/app.js'),
-  appOutput: path.resolve(dirname, 'dist/assets/js/')
+    appPoint: path.resolve(dirname, 'src/assets/js/app.js'),
+    appOutput: path.resolve(dirname, 'dist/assets/js/')
 };
 
 export default () => {
-  return {
-    entry: {
-      app: PATHS.appPoint
-    },
+    return {
+        entry: {
+            app: PATHS.appPoint
+        },
 
-    output: {
-      filename: 'app.build.js',
-      path: PATHS.appOutput
-    },
+        output: {
+            filename: 'app.build.js',
+            path: PATHS.appOutput
+        },
 
-    resolve: {
-      modules: ['node_modules']
-    },
+        resolve: {
+            modules: ['node_modules']
+        },
 
-    module: {
-      rules: [
-        {
-          test: /\.js$/i,
-          use: [
-            {
-              loader: 'cache-loader',
-              options: {
-                cacheDirectory: path.resolve(
-                  dirname,
-                  '.cache/js-cache'
-                )
-              }
-            },
-            'babel-loader'
-          ]
-        }
-      ]
-    },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/i,
+                    use: [
+                        {
+                            loader: 'cache-loader',
+                            options: {
+                                cacheDirectory: path.resolve(
+                                    dirname,
+                                    '.cache/js-cache'
+                                )
+                            }
+                        },
+                        'babel-loader'
+                    ]
+                }
+            ]
+        },
 
-    watchOptions: {
-      aggregateTimeout: 100
-    },
+        watchOptions: {
+            aggregateTimeout: 100
+        },
 
-    plugins: [
-      new ESLintPlugin()
-    ]
-  };
+        plugins: [
+            new ESLintPlugin()
+        ]
+    };
 };
